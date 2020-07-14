cbc0c5e010c048e01e4580a1cfd72b740eacf899
ArthurCurry <82575486@qq.com>
2020-07-14 19:03:34 +0800

    Revert "铺瓦片"
    This reverts commit 2316e6bfde5bda5b09bed187d02bfcb6eb5d4bd7.

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using System;
 
 public class TileType
 {
@@ -15,13 +13,12 @@ public class TileType
 public class TileManager {
 
     private static TileManager instance;
-    private Tilemap tilemap;
 
     public static TileManager Instance
     {
         get
         {
-            if (instance == null)
+            if(instance==null)
             {
                 instance = new TileManager();
             }
@@ -29,32 +26,13 @@ public class TileManager {
         }
     }
 
-    private TileManager()
-    {
-        InitData();
-    }
 
-
-    public void LayTiles(Map map,List<RoomNode> rooms,List<Vector2Int> corridors,Tilemap tilemap,System.Random seed)
-    {
-        foreach(RoomNode room in rooms)
-        {
-            for(int y =room.bottomLeft.y;y<=room.topRight.y;y++)
-            {
-                for(int x=room.bottomLeft.x;x<=room.topRight.x;x++)
-                {
-                    
-                }
-            }
-        }
-    }
-
-    private void LaySingleTile(TileType tileType,int x,int y,Tilemap tilemap)
+    public void LayTiles(Map map,List<RoomNode> rooms,List<Vector2Int> corridors)
     {
 
     }
 
-    private void InitData()
+    private void LaySingleTile(TileType tileType)
     {
 
     }
