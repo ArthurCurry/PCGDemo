df14b98212760cdefc39cea209fa59fb41bda518
ArthurCurry <82575486@qq.com>
2020-07-23 20:43:11 +0800

    自定义模块的自动更新

--- Assets/Scripts/Tile/ObstacleTile.cs
+++ Assets/Scripts/Tile/ObstacleTile.cs
@@ -42,8 +42,13 @@ public class ObstacleTile :TileBase {
 
     public override void RefreshTile(Vector3Int position, ITilemap tilemap)
     {
-        //Debug.Log("refreshed" + "  " + position+hps[position]);
+        //Debug.Log("refreshed" + "  " + position + hps[position]);
         base.RefreshTile(position, tilemap);
+    }
+
+    private void OnDestroy()
+    {
+        
     }
 
     public void UpdateSelf(Vector3Int position,int hp)
