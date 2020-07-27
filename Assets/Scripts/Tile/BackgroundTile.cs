31a0e9e99bddac3f75a112df87b403e5f5b21ac6
ArthurCurry <82575486@qq.com>
2020-07-27 20:32:56 +0800

    新的背景tile

---
+++ Assets/Scripts/Tile/BackgroundTile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu()]
+public class BackgroundTile :TileBase  {
+
+    public List<Sprite> wallRight;
+    public List<Sprite> wallLeft;
+    public List<Sprite> wallUp;
+    public List<Sprite> wallDown;
+    public Sprite defaultSprite;
+    public Sprite sprite;
+
+    public string seedCode;
+    private System.Random seed;
+
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+
+        base.GetTileData(position, tilemap, ref tileData);
+    }
+
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        base.RefreshTile(position, tilemap);
+    }
+
+    private void OnEnable()
+    {
+        this.seed = new System.Random(seedCode.GetHashCode());
+    }
+
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go)
+    {
+        return base.StartUp(position, tilemap, go);
+    }
+}
