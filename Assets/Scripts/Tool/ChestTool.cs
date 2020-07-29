b92ab29ad148abea9db31b43da5c90afb6008656
ArthurCurry <82575486@qq.com>
2020-07-29 18:28:07 +0800

    真的下班了

--- Assets/Scripts/Tool/ChestTool.cs
+++ Assets/Scripts/Tool/ChestTool.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
-public class ChestTool : MonoBehaviour {
+public class ChestTool : Tool {
+
+    public List<Tool> gadgets;
+    public List<RuntimeAnimatorController> controllers;
+    public System.Random seed;
+    private Animator animator;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +21,9 @@ public class ChestTool : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void InitData()
+    {
+        animator.runtimeAnimatorController = controllers[seed.Next(0, controllers.Count)];
+    }
 }
--- Assets/Scripts/Tool/Tool.cs
+++ Assets/Scripts/Tool/Tool.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ToolPotion : Tool {
+public class Tool : MonoBehaviour {
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +13,6 @@ public class ToolPotion : Tool {
 	
 	// Update is called once per frame
 	void Update () {
-        //Debug.Log(this.gameObject.name);
+		
 	}
 }
