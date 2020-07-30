d048d6af9770239538ef17b29b962226fc740f7e
ArthurCurry <82575486@qq.com>
2020-07-30 11:17:23 +0800

    新素材，攻击脚本

--- Assets/Scripts/Tool/ChestTool.cs
+++ Assets/Scripts/Tool/ChestTool.cs
@@ -6,8 +6,9 @@ using UnityEngine.Animations;
 public class ChestTool : Tool {
 
     public List<Tool> gadgets;
-    public List<RuntimeAnimatorController> controllers;
+    public List<AnimatorOverrideController> controllers;
     public System.Random seed;
+    public string seedCode;
     private Animator animator;
     private Tool gadget;
     
@@ -21,7 +22,7 @@ public class ChestTool : Tool {
 
     // Use this for initialization
     void Start () {
-		
+        InitData();
 	}
 	
 	// Update is called once per frame
@@ -31,19 +32,26 @@ public class ChestTool : Tool {
 
     public void InitData()
     {
+        seed = new System.Random(transform.position.GetHashCode());
         animator = this.GetComponent<Animator>();
         animator.runtimeAnimatorController = controllers[seed.Next(0, controllers.Count)];
+        //Debug.Log(animator.runtimeAnimatorController.name);
+
         gadget = gadgets[seed.Next(0, gadgets.Count)];
     }
 
     public void Hit()
     {
         hp -= 1;
-        animator.SetTrigger("hit");
+        if (hp <= 0)
+            animator.SetTrigger("destroyed");
+        else
+            animator.SetTrigger("hit");
     }
 
     public void DestroySelf()
     {
+        
         GameObject.Instantiate(gadget,transform.position,gadget.transform.rotation);
         Destroy(this.gameObject);
     }
